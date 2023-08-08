1151e2d7535b4a39ccb9db84e8eec6b860a1f5eb
sunny park <sunnyparkk@gmail.com>
2023-08-08 19:25:29 -0400

    Change test into it

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import Header from "../Header";
 
-test("renders learn react link", () => {
+it("should render same text passed into title prop", () => {
 	render(<Header title="My Header" />);
 	const headingElement = screen.getByText(/my header/i);
 	expect(headingElement).toBeInTheDocument();
